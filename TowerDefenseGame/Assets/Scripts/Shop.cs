7cd15455e8f531224af2c6aebebd495a7767dd37
Kevin Pratt <redhotkbp@hotmail.com>
2022-04-24 11:08:38 -0500

    add missle launcher

--- TowerDefenseGame/Assets/Scripts/Shop.cs
+++ TowerDefenseGame/Assets/Scripts/Shop.cs
@@ -15,9 +15,9 @@ public class Shop : MonoBehaviour
         buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
     }
 
-    public void PurchaseAnotherTurret()
+    public void PurchaseMissileLauncher()
     {
-        Debug.Log("Another Turret Selected");
-        buildManager.SetTurretToBuild(buildManager.anotherTurretPrefab);
+        Debug.Log("Missle Launcher Selected");
+        buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab);
     }
 }
