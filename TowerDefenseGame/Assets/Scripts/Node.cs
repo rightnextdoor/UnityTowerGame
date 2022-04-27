f7086dcc1b8d90b7d7619dd11867b9bfb94eac0e
Kevin Pratt <redhotkbp@hotmail.com>
2022-04-27 12:56:05 -0500

    added upgrade

--- TowerDefenseGame/Assets/Scripts/Node.cs
+++ TowerDefenseGame/Assets/Scripts/Node.cs
@@ -9,8 +9,12 @@ public class Node : MonoBehaviour
 
     public Vector3 poositionOffset;
 
-    [Header("Optional")]
+    [HideInInspector]
     public GameObject turret;
+    [HideInInspector]
+    public TurretBlueprint turretBlueprint;
+    [HideInInspector]
+    public bool isUpgraded = false;
 
     private Renderer rend;
 
@@ -43,7 +47,49 @@ public class Node : MonoBehaviour
         if (!buildManager.CanBuild)
             return;
 
-        buildManager.BuildTurretOn(this);
+        BuildTurret(buildManager.GetTurretToBuild());
+    }
+
+    void BuildTurret(TurretBlueprint blueprint)
+    {
+        if (PlayerStats.Money < blueprint.cost)
+        {
+            Debug.Log("Not enough money!");
+            return;
+        }
+        PlayerStats.Money -= blueprint.cost;
+
+        GameObject _turret = (GameObject)Instantiate(blueprint.prefab, GetBuildPosition(), Quaternion.identity);
+        turret = _turret;
+
+        turretBlueprint = blueprint;
+
+        GameObject effect = (GameObject)Instantiate(buildManager.buildEffect, GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
+        Debug.Log("Turret is build");
+    }
+
+    public void UpgradeTurret()
+    {
+        if (PlayerStats.Money < turretBlueprint.upgradeCost)
+        {
+            Debug.Log("Not enough money to upgrade!");
+            return;
+        }
+        PlayerStats.Money -= turretBlueprint.upgradeCost;
+
+        //Get rid of the old turret
+        Destroy(turret);
+
+        //Build a new one
+        GameObject _turret = (GameObject)Instantiate(turretBlueprint.upgradedPrefab, GetBuildPosition(), Quaternion.identity);
+        turret = _turret;
+
+        GameObject effect = (GameObject)Instantiate(buildManager.buildEffect, GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
+
+        isUpgraded = true;
+        Debug.Log("Turret is Upgraded");
     }
 
     private void OnMouseEnter()
--- TowerDefenseGame/Assets/Scripts/NodeUI.cs
+++ TowerDefenseGame/Assets/Scripts/NodeUI.cs
@@ -1,20 +1,41 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class NodeUI : MonoBehaviour
 {
     private Node target;
     public GameObject ui;
+    public TextMeshProUGUI upgradeCostText;
+    public Button upgradeButton;
 
     public void SetTarget(Node _target)
     {
         target = _target;
 
         transform.position = target.GetBuildPosition();
+        if (!target.isUpgraded)
+        {
+            upgradeCostText.text = "$" + target.turretBlueprint.upgradeCost;
+            upgradeButton.interactable = true;
+        }
+        else
+        {
+            upgradeCostText.text = "DONE";
+            upgradeButton.interactable = false;
+        }
+        
         ui.SetActive(true);
     }
 
     public void Hide()
     {
         ui.SetActive(false);
+    }
+
+    public void Upgrade()
+    {
+        target.UpgradeTurret();
+        BuildManager.instance.DeselectNode();
     }
 }
