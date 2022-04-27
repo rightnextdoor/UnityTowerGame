9f893d3c6dd32f4e8792cc5747eb573b66c26d19
Kevin Pratt <redhotkbp@hotmail.com>
2022-04-27 11:20:50 -0500

    added nodeUI

--- TowerDefenseGame/Assets/Scripts/BuildManager.cs
+++ TowerDefenseGame/Assets/Scripts/BuildManager.cs
@@ -5,6 +5,8 @@ public class BuildManager : MonoBehaviour
     public static BuildManager instance;
 
     private TurretBlueprint turretToBuild;
+    private Node selectedNode;
+    public NodeUI nodeUI;
 
     public GameObject buildEffect;
 
@@ -24,7 +26,28 @@ public class BuildManager : MonoBehaviour
     public void SelectTurretToBuild(TurretBlueprint turret)
      {
         turretToBuild = turret;
+        DeselectNode();
      }
+
+    public void SelectNode(Node node)
+    {
+        if(selectedNode == node)
+        {
+            DeselectNode();
+            return;
+        }
+
+        selectedNode = node;
+        turretToBuild = null;
+
+        nodeUI.SetTarget(node);
+    }
+
+    public void DeselectNode()
+    {
+        selectedNode = null;
+        nodeUI.Hide();
+    }
 
     public void BuildTurretOn(Node node)
     {
--- TowerDefenseGame/Assets/Scripts/Node.cs
+++ TowerDefenseGame/Assets/Scripts/Node.cs
@@ -34,14 +34,15 @@ public class Node : MonoBehaviour
         if (EventSystem.current.IsPointerOverGameObject())
             return;
 
+        if(turret != null)
+        {
+            buildManager.SelectNode(this);
+            return;
+        }
+
         if (!buildManager.CanBuild)
             return;
 
-        if(turret != null)
-        {
-            Debug.Log("Can't build there! = TODO: Display on screen.");
-            return;
-        }
         buildManager.BuildTurretOn(this);
     }
 
