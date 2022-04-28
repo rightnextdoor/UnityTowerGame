1152d3fcbed6b6c62f6d6258dd35e20d147e7a17
Kevin Pratt <redhotkbp@hotmail.com>
2022-04-28 15:15:31 -0500

    added pause menu

---
+++ TowerDefenseGame/Assets/Scripts/PausedMenu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausedMenu : MonoBehaviour
+{
+    public GameObject ui;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        } else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        Toggle();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        Debug.Log("Go to Menu");
+    }
+}
